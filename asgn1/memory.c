85e509fd2e4458b7ca288e6402b88d6b2d173332
bijaya khadka <bkhadka@ucsc.edu>
2023-01-19 12:54:01 -0800

    worked on set command

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -43,9 +43,13 @@ int main() {
         for (i = 0; i < strlen(buff1); i++)
             printf("\nb:'%c'\n", buff1[i]);
 
-        int o = open("test.txt", O_RDONLY);
+        int o = open(buff1, O_RDONLY);
 
         printf("o: %d", o);
+        if (o < 0) {
+            write(1, "Invalid command", sizeof("Invalid command"));
+            return 1;
+        }
         int fr = 0;
         int rd;
         int counter = 0;
@@ -67,8 +71,54 @@ int main() {
             printf("\nbuff2: %s\n", buff2);
         }
         printf("\n%s\n", buff2);
-    } else if (strcmp(buff, "set") == 0) {
+        return 0;
+    } else if (strcmp(buff, "set ") == 0) {
         write(1, buff, 4);
+
+        while (c[0] != '\n') {
+            *c = '\0';
+
+            //printf("\nC: %s", c);
+            read(0, c, 1);
+            if (c[0] == '\n')
+                break;
+            //printf("\nC: %s", c);
+            strncat(buff1, c, 1);
+            s++;
+            buff1 = realloc(buff1, s * sizeof(char));
+
+            //printf("buff1: %s", buff1);
+
+            //printf("while:  %c", c[0]);
+        }
+        printf("buff1: %s", buff1);
+        int o = open(buff1, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+
+        printf("o: %d", o);
+
+        *c = '\0';
+        int counter = 0;
+
+        while (c[0] != '\n') {
+            *c = '\0';
+
+            //printf("\nC: %s", c);
+            read(0, c, 1);
+            if (c[0] == '\n')
+                break;
+            //printf("\nC: %s", c);
+            strncat(buff2, c, 1);
+            counter++;
+            buff2 = realloc(buff2, counter + counter * sizeof(char));
+
+            //printf("buff1: %s", buff1);
+
+            //printf("while:  %c", c[0]);
+        }
+
+        printf("buff2: %s", buff2);
+        write(o, buff2, strlen(buff2));
+
     } else {
         write(1, "Invalid command", sizeof("Invalid command"));
     }
